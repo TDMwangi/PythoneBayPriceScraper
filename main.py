7c5efdd1c107d7b4628922676f05b9e8ccee9167
TDMwangi <teddymwangi65@gmail.com>
2023-10-06 20:43:41 +0300

    feat: get ebay data

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 searchterm = 'iphone'
+
+def get_data(searchterm):
+    url = f'https://www.ebay.co.uk/sch/i.html?_from=R40&_nkw={searchterm}&_sacat=0&LH_PrefLoc=1&LH_Auction=1&rt=nc&LH_Sold=1&LH_Complete=1'
