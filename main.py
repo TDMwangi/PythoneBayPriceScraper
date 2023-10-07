a4efca93b61507c2d98c5b74e4bcc7bd576f246b
TDMwangi <teddymwangi65@gmail.com>
2023-10-07 20:47:03 +0300

    chore: add parser

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ searchterm = 'iphone'
 
 def get_data(searchterm):
     url = f'https://www.ebay.co.uk/sch/i.html?_from=R40&_nkw={searchterm}&_sacat=0&LH_PrefLoc=1&LH_Auction=1&rt=nc&LH_Sold=1&LH_Complete=1'
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    return soup
